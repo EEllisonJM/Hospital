d5af1ae38e7addd27556ec893d7e290763a90967
EEllisonJM <ellison_jm@hotmail.com>
2017-11-22 23:06:57 -0600

    index modificado
    - Funcion modo offline de Bootstrap
    - Creacion de css "miCSS", separando el css en index.php
    - Agregando iconos a los navegadores

--- index.php
+++ index.php
@@ -1,79 +1,25 @@
-
-<!DOCTYPE html>
+<!DOCTYPE html><!-- Declaración del tipo de documento => Versión de HTML a trabajar => HTML Versión 5-->
 <html lang="es-ES">
-
 <head>
-  <meta charset="UTF-8">
+  <meta charset="UTF-8"><!-- Permiete representar caracteres de varios paises (tildes, acentos ...) -->
   <title>Hospital</title>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous"> 
-<style type="text/css">
-        hr  {
-            border:10px solid #000;
-            border-color: #A7DBD8;
-        }
-        h1 {
-          color: #5bc0de;
-          font-family: Times;
-        }
-
-.slider{
-	width: 50%;
-	margin: auto; 
-	overflow: hidden;
-}
-
-.slider ul{
-	display: flex;
-	padding: 0;
-	width: 400%;
-
-	animation: cambio 20s infinite;
-	animation-direction: alternate;
-	animation-timing-function: ease-in;
-}
-
-.slider li{
-	width: 100%;
-	list-style: none;
-}
-
-.slider img{
-	width: 100%;
-}
-@keyframes cambio{
-0% {margin-left: 0;}
-20% {margin-left: 0;}
-
-25% {margin-left: -100%;}
-45% {margin-left: -100%;}
-
-50% {margin-left: -200%;}
-70% {margin-left: -200%;}
-
-75% {margin-left: -300%;}
-100% {margin-left: -300%;}
-}
-
-
-</style>
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/miCSS.css">
 </head>
 
-
 <body>
-
-
-<nav class="navbar navbar-inverse">
-
-   <ul class="nav navbar-nav navbar-left">
-      <li><a href="nosotros.php"><span class="glyphicon"></span> Nosotros </a></li>
+  <nav class="navbar navbar-inverse">
+    <ul class="nav navbar-nav navbar-left">
+      <li><a href="nosotros.php"><span class="glyphicon glyphicon-globe"></span> Nosotros </a></li>
       <a class="navbar-brand">|</a>
-      <li><a href="contactos.php"><span class="glyphicon"></span> Contactos  </a></li>
+
+      <li><a href="contactos.php"><span class="glyphicon glyphicon-phone-alt"></span> Contactos  </a></li>
       <a class="navbar-brand">   </a>
       <a class="navbar-brand">|</a>
-        <li><a href="index.php"><span class="glyphicon glyphicon-home"></span> Inicio  </a></li>
-      <a class="navbar-brand">   </a>
 
-  </ul>  
+      <li><a href="index.php"><span class="glyphicon glyphicon-home"></span> Inicio  </a></li>
+      <a class="navbar-brand"></a>
+  </ul>
 
   <ul class="nav navbar-nav navbar-right">
       <li><a href="../Hospital/ses/logueo.php"><span class="glyphicon glyphicon-user"></span> Iniciar sesion</a></li>
@@ -93,9 +39,5 @@
 	</ul>
 </div>
 
-  <p> 
-      
-  </p>
 </body>
-
 </html>
