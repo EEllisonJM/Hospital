071bc3878812a03e49ef67679623a35411b5e7e2
EEllisonJM <ellison_jm@hotmail.com>
2017-12-07 03:17:08 -0600

    Login corregido
    Para cargar los datos al servidor, es necesario que los campos tengan el mismo tipo de Letra (El servidor Diferencia Mayuscusculas de Minusculas).
    Con esto soluciono un problema con el Login sin afectar al que tenemos.

--- ses/login.php
+++ ses/login.php
@@ -17,34 +17,34 @@ include("../configuraciondb.php");
   //}
        else 
        {
-            $r = mysqli_query($conexion,"SELECT idUsuario, idEmpleado, idpuesto, userName, password, tipoempleado FROM usuario NATURAL JOIN empleado NATURAL JOIN puesto WHERE userName='".$_REQUEST["user"]."'
-			                  and password='".$_REQUEST["pass"]."'");
+            $r = mysqli_query($conexion,"SELECT idUsuario, idEmpleado, idPuesto, userName, password, tipoEmpleado FROM Usuario NATURAL JOIN Empleado NATURAL JOIN Puesto WHERE userName='".$_REQUEST["user"]."'
+                        and password='".$_REQUEST["pass"]."'");
             $r2 = mysqli_fetch_array($r);
-			
-	        $r1 = mysqli_num_rows($r);
+      
+          $r1 = mysqli_num_rows($r);
 
             if($r1 == 1) //comparacion donde tiene que mostrar solo 1 registro (usuario nunca se debe de repetir)
             {
-				$_SESSION["usuario"] = $r2["userName"]; // declaracion de la variable de sesion usuario
-				$_SESSION["tipo"] = $r2["tipoempleado"]; // declaracion de la variable de sesion tipo de usuario
-				//Te refirige a la vista
-				if($_SESSION["tipo"]=="ADMINISTRADOR"||$_SESSION["tipo"]=="GERENTE"||$_SESSION["tipo"]=="JEFE DE AREA"||$_SESSION["tipo"]=="ENCARGADO DE FARMACIA"||$_SESSION["tipo"]=="JEFE DE RECURSOS HUMANOS"){
-					?>
-					<script language="javascript">
-					alert("Te haz logueado satisfactoriamente");
-					window.location="../administrador/admin.php";
-					</script>
-					<?php
-				}
-       	    }
-			else
+        $_SESSION["usuario"] = $r2["userName"]; // declaracion de la variable de sesion usuario
+        $_SESSION["tipo"] = $r2["tipoEmpleado"]; // declaracion de la variable de sesion tipo de usuario
+        //Te refirige a la vista
+        if($_SESSION["tipo"]=="ADMINISTRADOR"||$_SESSION["tipo"]=="GERENTE"||$_SESSION["tipo"]=="JEFE DE AREA"||$_SESSION["tipo"]=="ENCARGADO DE FARMACIA"||$_SESSION["tipo"]=="JEFE DE RECURSOS HUMANOS"){
+          ?>
+          <script language="javascript">
+          alert("Te haz logueado satisfactoriamente");
+          window.location="../administrador/admin.php";
+          </script>
+          <?php
+        }
+            }
+      else
             {
-			?>
+      ?>
                  <script language="javascript">
                  alert("Error, Escriba correctamente su Usuario y/o Password");
                  window.location="logueo.php";
                  </script> 
-			<?php
+      <?php
             }
       }
 ?>
