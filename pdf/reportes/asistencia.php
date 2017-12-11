c37e8ed44bb58e1d7f46ce4ee1f383b2e43a9666
Avenging02 <32710195+Avenging02@users.noreply.github.com>
2017-12-10 18:17:11 -0600

    reporte con nombre de area
    puedes ver este porfa

--- pdf/reportes/asistencia.php
+++ pdf/reportes/asistencia.php
@@ -3,10 +3,20 @@
 require_once('../lib/pdf/mpdf.php');
 include ("../../configuraciondb.php");
 if($_REQUEST['perfecta'] == "perfecta") {
-$query = "SELECT recursoshumanos.idEmpleado, empleado.nombre, empleado.apellidoP, empleado.apellidoM, recursoshumanos.idArea, asistencia.fecha, recursoshumanos.horaEntrada, recursoshumanos.horaSalida FROM empleado INNER JOIN recursoshumanos ON empleado.idEmpleado = recursoshumanos.idEmpleado INNER JOIN asistencia ON asistencia.idEmpleado=recursoshumanos.idEmpleado AND asistencia.horaEntra <= recursoshumanos.horaEntrada AND Asistencia.horaEntra <= RecursosHumanos.horaEntrada";
+$query = "SELECT recursoshumanos.idEmpleado, empleado.nombre, empleado.apellidoP, empleado.apellidoM, area.nombre, asistencia.fecha, recursoshumanos.horaEntrada, recursoshumanos.horaSalida 
+FROM empleado 
+INNER JOIN recursoshumanos ON empleado.idEmpleado = recursoshumanos.idEmpleado 
+INNER JOIN asistencia ON asistencia.idEmpleado=recursoshumanos.idEmpleado 
+INNER JOIN area ON area.idArea=recursoshumanos.idArea
+AND asistencia.horaEntra <= recursosHumanos.horaEntrada";
 }else{
 if($_REQUEST['inperfecta'] == "inperfecta"){
-$query = "SELECT recursoshumanos.idEmpleado, empleado.nombre, empleado.apellidoP, empleado.apellidoM, recursoshumanos.idArea, asistencia.fecha, recursoshumanos.horaEntrada, recursoshumanos.horaSalida FROM empleado INNER JOIN recursoshumanos ON empleado.idEmpleado = recursoshumanos.idEmpleado INNER JOIN asistencia ON asistencia.idEmpleado=recursoshumanos.idEmpleado AND asistencia.horaEntra <= recursoshumanos.horaEntrada AND Asistencia.horaEntra >= RecursosHumanos.horaEntrada";
+$query = "SELECT recursoshumanos.idEmpleado, empleado.nombre, empleado.apellidoP, empleado.apellidoM, area.nombre, asistencia.fecha, recursoshumanos.horaEntrada, recursoshumanos.horaSalida 
+FROM empleado 
+INNER JOIN recursoshumanos ON empleado.idEmpleado = recursoshumanos.idEmpleado 
+INNER JOIN asistencia ON asistencia.idEmpleado=recursoshumanos.idEmpleado 
+INNER JOIN area ON area.idArea=recursoshumanos.idArea
+AND asistencia.horaEntra > recursosHumanos.horaEntrada";
 }
 }
 $prepare = $conexion->prepare($query);
