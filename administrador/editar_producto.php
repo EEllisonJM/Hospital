91a0a05f6c033a44ad2af696bcedf82f4cf1eca0
Avenging02 <32710195+Avenging02@users.noreply.github.com>
2017-12-12 07:50:17 -0600

    Validar ses de productos
    Validar sesion de productos editar y eliminar

--- administrador/editar_producto.php
+++ administrador/editar_producto.php
@@ -3,6 +3,7 @@ session_start();
 ob_start();
 error_reporting(E_ALL ^ E_NOTICE); //no mostrar errores de sintaxis
 include("menu.php");
+if ($_SESSION["tipo"] == "ADMINISTRADOR" ||  $_SESSION["tipo"] == "ENCARGADO DE FARMACIA") {
 ?>
 <html>
 <script src="../angular.min.js"></script>
@@ -138,3 +139,16 @@ $scope.delete = function(tableData){
 </div>
 </body>
 </html>
+<?php
+
+}
+// cuando no este logueado (iniciado sesion) mostrara la siguiente alerta de acceso denegado y redireccionara al login de inicio de sesion
+else {
+?>
+  <script>
+       alert("Acceso Denegado");
+       window.location = "../ses/logueo.php";
+   </script>
+<?php
+}
+?> 
